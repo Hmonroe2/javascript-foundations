0d0bd6413a8e6af50c504a37f10eb2ba65d0d560
Hunter Monroe <huntis@outlook.com>
2022-07-21 09:06:52 -0600

    finished snowman

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,28 @@
 var Snowman = require('./Snowman');
 
 class Human {
+  constructor(name) {
+    this.name = name
+    this.wantsToBuildASnowman = true
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
 
+  }
+  gatherMaterials(name, amount) {
+    this.materials[name] += amount
+  }
+  buildASnowman(){
+    var snowman = new Snowman(this.materials)
+    return snowman
+  }
+  placeMagicHat(){
+    var snowman = this.buildASnowman()
+    return snowman.canWearMagicHat() ? `Woah, this snowman is coming to life!`:`I guess I didn't build it correctly.`
+  }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,17 @@
 class Snowman {
-
+  constructor(data){
+    this.carrots = data.carrots
+    this.coal = data.coal
+    this.buttons = data.buttons
+    this.snowballs = data.snowballs
+    this.magicHat = false
+  }
+  canWearMagicHat(){
+    if(this.coal > 1 && this.buttons > 4 && this.carrots > 0 && this.snowballs > 2){
+    this.magicHat = true
+    return true
+  }
+  return false
 }
-
+}
 module.exports = Snowman;
